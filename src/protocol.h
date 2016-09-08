1f2d4ce63053339d5e27cfcc73262e9b33ceeae6
Arjun Mishra <arjun@penny.partners>
2016-09-09 02:30:07 +0530

    rpc changes

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 22675 : 12675;
+    return testnet ? 22675 : 12673;
 }
 
 
